7b8d63fb3ce54e661e6d261dde22c978b25d940f
EricaReis <ericareis67@outlook.com>
2023-02-24 11:27:13 -0300

    Updating routes to new pattern

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,13 +1,20 @@
-import Router from "../shared/routes/routes";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import store from "../data/store/store";
+import { AppRoutes } from "../shared/routes/AppRoutes";
 import GlobalStyles from "../shared/styles/GlobalStyles";
+import { defaultTheme } from "../shared/styles/themes/default.theme";
 
 function App() {
   return (
     <Provider store={store}>
       <GlobalStyles />
-      <Router />
+      <ThemeProvider theme={defaultTheme}>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   );
 }
